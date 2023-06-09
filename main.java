2b9c6c7fdc0da425aa8bcab666db9f17766db076
HildaDeliana <115686134+HildaDeliana@users.noreply.github.com>
2023-06-09 18:16:29 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,19 @@
+
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Online IDE!! Happy Coding :)");
+    }
+}
+import java.util.Scanner;
+
+public class Main {
+    public static int faktorial(int a) {
+        if (a == 1) {
+            
